25a4e88ee8968c4ad1d9c5eb6ae9e05b39b5e42f
gwang <wanggy@outlook.com>
2020-09-16 14:35:00 -0700

    fix

--- tools/ci_build/github/linux/ort_minimal/build_full_ort_and_create_ort_file.sh
+++ tools/ci_build/github/linux/ort_minimal/build_full_ort_and_create_ort_file.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e -o -x
 
-/opt/python/cp37-cp37m/bin/python3 /onnxruntime_src/tools/ci_build/build.py \
-    --build_dir /build --cmake_generator Ninja \
-    --config Debug \
-    --skip_submodule_sync \
-    --parallel \
-    --build_wheel \
-    --skip_tests \
-    --enable_pybind \
-    --cmake_extra_defines PYTHON_INCLUDE_DIR=/opt/python/cp37-cp37m/include/python3.7m PYTHON_LIBRARY=/usr/lib64/librt.so
+onnxruntimeregistry.azurecr.io/internal/azureml/onnxruntimecpubuild:ch36 \
+    /opt/python/cp37-cp37m/bin/python3 /onnxruntime_src/tools/ci_build/build.py \
+        --build_dir /build --cmake_generator Ninja \
+        --config Debug \
+        --skip_submodule_sync \
+        --parallel \
+        --build_wheel \
+        --skip_tests \
+        --enable_pybind \
+        --cmake_extra_defines PYTHON_INCLUDE_DIR=/opt/python/cp37-cp37m/include/python3.7m PYTHON_LIBRARY=/usr/lib64/librt.so
 
 /opt/python/cp37-cp37m/bin/python3  -m pip install -U /build/Debug/dist/*
 
