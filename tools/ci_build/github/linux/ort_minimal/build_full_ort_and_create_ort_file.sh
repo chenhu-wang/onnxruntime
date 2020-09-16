d37d45cf198ff619c286c639fe17ec25b6248222
gwang <wanggy@outlook.com>
2020-09-16 15:22:54 -0700

    fix

--- tools/ci_build/github/linux/ort_minimal/build_full_ort_and_create_ort_file.sh
+++ tools/ci_build/github/linux/ort_minimal/build_full_ort_and_create_ort_file.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e -o -x
 
+EXIT_CODE=1
+
+# run a full build of ORT
 /opt/python/cp37-cp37m/bin/python3 /onnxruntime_src/tools/ci_build/build.py \
     --build_dir /build --cmake_generator Ninja \
     --config Debug \
@@ -11,7 +14,16 @@ set -e -o -x
     --enable_pybind \
     --cmake_extra_defines PYTHON_INCLUDE_DIR=/opt/python/cp37-cp37m/include/python3.7m PYTHON_LIBRARY=/usr/lib64/librt.so
 
+# install the ORT python wheel
 /opt/python/cp37-cp37m/bin/python3  -m pip install -U /build/Debug/dist/*
+
+# copy the test data to a separated folder
+mkdir -p /home/onnxruntimedev/.test_data
+cp -Rf $(Build.SourcesDirectory)/onnxruntime/test/testdata/ort_minimal_e2e_test_data /home/onnxruntimedev/.test_data
+
+# convert the onnx models the $HOME/.test_data to ort model
+find /home/onnxruntimedev/.test_data -type f -name "*.onnx"  -exec /opt/python/cp37-cp37m/bin/python3 \
+    /onnxruntime_src/tools/python/convert_onnx_model_to_ort.py {} \;
 
 EXIT_CODE=$?
 
